5bad2f05d531794dfc77833eefad60e6fb0cb6c6
DeepsDali <deepashri.dali@gmail.com>
2023-07-24 14:06:44 +0100

    Add more primitive values tests including NaN

--- assert-equals.js
+++ assert-equals.js
@@ -1,4 +1,10 @@
 const assertEquals = (expected, actual) => {
+  // Check for NaN values
+  if (Number.isNaN(expected) && Number.isNaN(actual)) {
+    // NaN values are considered equal
+    return;
+  }
+
   // Check Primitive types
   if (expected !== actual) {
     throw new Error(`Expected ${expected} but received ${actual}`);
--- assert-equals.spec.js
+++ assert-equals.spec.js
@@ -7,7 +7,25 @@ describe("assertEquals", () => {
     it("should not throw an error for equal primitive values", () => {
       expect(() => assertEquals("Hello", "Hello")).not.toThrow(); //string
       expect(() => assertEquals(45000, 45000)).not.toThrow(); //number
+      expect(() => assertEquals(-45000, -45000)).not.toThrow(); //negative number
+      expect(() => assertEquals(3.14, 3.14)).not.toThrow(); //float (Pi)
+      expect(() => assertEquals(true, true)).not.toThrow(); //Boolean
+      expect(() => assertEquals(null, null)).not.toThrow(); // Null
+      expect(() => assertEquals(undefined, undefined)).not.toThrow(); //undefined
+      expect(() => assertEquals(NaN, NaN)).not.toThrow(); //NaN
+      expect(() => assertEquals(0, 0)).not.toThrow(); //0
+      expect(() => assertEquals("", "")).not.toThrow(); // Empty string
+      expect(() =>
+        assertEquals(9007199254740991, 9007199254740991)
+      ).not.toThrow(); //BigInt
+      expect(() =>
+        assertEquals(
+          "!@#$%^&*()_+-=[]{}|;:,.<>?/",
+          "!@#$%^&*()_+-=[]{}|;:,.<>?/"
+        )
+      ).not.toThrow(); // Special characters
     });
+
     //Compare same Types
     //Compare same Regex patterns
     //Compare same Arrays (Lengths and Elements)
@@ -18,6 +36,20 @@ describe("assertEquals", () => {
     it("should throw an error for unequal primitive values", () => {
       expect(() => assertEquals("Hello", "World")).toThrow(); //string
       expect(() => assertEquals(45000, 4500)).toThrow(); //number
+      expect(() => assertEquals(-45000, -450000)).toThrow(); //negative number
+      expect(() => assertEquals(3.141, 3.14)).toThrow(); //float (Pi)
+      expect(() => assertEquals(true, false)).toThrow(); //Boolean
+      expect(() => assertEquals(null, undefined)).toThrow(); // Null and undefined
+      expect(() => assertEquals(NaN, 0)).toThrow(); //NaN 0
+      expect(() => assertEquals(1, "1")).toThrow(); //string and number
+      expect(() => assertEquals("", " ")).toThrow(); // Different empty string
+      expect(() => assertEquals(9007199254740990, 9007199254740991)).toThrow(); //BigInt
+      expect(() =>
+        assertEquals(
+          "!@#$%^&*()_+-=[]{}|;:,.<>?/",
+          "@#$%^&*()_+-=[]{}|;:,.<>?!/"
+        )
+      ).toThrow(); // Special characters
     });
     //Compare unequal Types
     //Compare different Regex patterns
